cf3db5eb6728ff42ef311892ecc3f0135451e8e4
Самойлов Евгений Александрович <easamoilov@gmail.com>
2020-09-08 20:06:24 +0300

    add like

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,29 @@
+const initialCards = [{
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
 const popupManager = {
   page: document.querySelector(".page"),
   popup: document.querySelector(".popup"),
@@ -47,9 +73,39 @@ const profileFormManager = {
   getName: function getName() {
     return this.nameInput.value;
   },
-  
+
   getJob: function getName() {
     return this.jobInput.value
+  }
+}
+
+const elementsManager = {
+  elementsContainer: document.querySelector(".elements"),
+  elementTemplate: document.querySelector("#template-element"),
+
+  addElement: function addElement(element) {
+    this.elementsContainer.append(element);
+  },
+
+  createFromTemplate: function createFromTemplate(data) {
+    const element = this.elementTemplate.content.cloneNode(true);
+    element.querySelector(".element__title").textContent = data.name;
+
+    const elementImage = element.querySelector(".element__image");
+    elementImage.src = data.link;
+    elementImage.alt = `Картинка '${data.name}'`;
+
+    const deleteButton = element.querySelector(".element__delete");
+
+    element.querySelector(".element__delete").addEventListener('click', (evt) => {
+      evt.target.parentNode.remove();
+    });
+
+    element.querySelector(".element__like").addEventListener('click', (evt) => {
+      evt.target.classList.toggle("icon-button_type_like-active");
+    });
+
+    return element;
   }
 }
 
@@ -70,4 +126,13 @@ function initSubscriptions() {
   });
 }
 
+
+function loadElements() {
+  initialCards.forEach(x => {
+    let element = elementsManager.createFromTemplate(x);
+    elementsManager.addElement(element);
+  });
+}
+
 initSubscriptions();
+loadElements();
